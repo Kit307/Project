753cd28b50b60e9f708ff4a38c34f49671471aa0
Kit307 <kintlove555@gmail.com>
2022-03-17 15:00:33 +0700

    add card page

--- src/components/CartTable.vue
+++ src/components/CartTable.vue
@@ -1,10 +1,188 @@
 <template>
   <div class="py-6"></div>
-  <h1>sdad</h1>
+  <div
+    wire:loading
+    v-show="!show"
+    class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center"
+  >
+    <svg
+      role="status"
+      class="mr-2 w-36 h-36 text-gray-200 animate-spin fill-blue-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
+    <h2 class="text-center text-white text-xl font-semibold">Loading...</h2>
+    <p class="w-1/3 text-center text-white">
+      This may take a few seconds, please don't close this page.
+    </p>
+  </div>
+
+  <div class="bg-white p-8 rounded-md w-full">
+    <div>
+      <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+        <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+          <table class="min-w-full leading-normal">
+            <thead>
+              <tr>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                >
+                  products
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                >
+                  Created at
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                >
+                  QRT
+                </th>
+                <th
+                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                >
+                  Status
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in producttable" :key="index">
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <div class="ml-3">
+                    <div class="flex items-center">
+                      <div class="flex-shrink-0 w-10 h-10">
+                        <img
+                          class="w-full h-full rounded-full"
+                          :src="item.img"
+                          alt=""
+                        />
+                      </div>
+                      <div class="ml-3">
+                        <p class="text-gray-900 whitespace-no-wrap">
+                          {{ item.data.title }}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <p class="text-gray-900 whitespace-no-wrap">
+                    {{ item.data.price }} $
+                  </p>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <p class="text-gray-900 whitespace-no-wrap">
+                    {{ item.count }}
+                  </p>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <p class="text-gray-900 whitespace-no-wrap">
+                    {{ item.data.price * item.count }} $
+                  </p>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                  <button
+                    type="button"
+                    class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg py-2 px-5 text-xs dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600"
+                  >
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div
+            class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
+          >
+            <span class="text-xs xs:text-sm text-gray-900">
+              Showing 1 to 4 of 50 Entries
+            </span>
+            <div class="inline-flex mt-2 xs:mt-0">
+              <button
+                class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l"
+              >
+                Prev
+              </button>
+              &nbsp; &nbsp;
+              <button
+                class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r"
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- {{ cart }} -->
+  <!-- {{ profiledata[0].data.data }} -->
+  <!-- {{ profiledata[0].id }}
+   -->
+  <!-- {{ profiledata[0].data.linkfile }} -->
 </template>
 
 <script>
-export default {};
+import { doc, onSnapshot } from "firebase/firestore";
+import { auth, db } from "../plugin/index.js";
+import { collection, getDocs } from "firebase/firestore";
+export default {
+  async mounted() {
+    await this.readdataincart();
+    const querySnapshot = await getDocs(collection(db, "product"));
+    querySnapshot.forEach((doc) => {
+      this.profiledata.push({ id: doc.id, data: doc.data() });
+    });
+    this.addProducttoTable();
+    this.show = true;
+  },
+  data() {
+    return {
+      cart: [],
+      show: false,
+      profiledata: [],
+      producttable: [],
+    };
+  },
+  methods: {
+    readdataincart() {
+      const user = auth.currentUser;
+      onSnapshot(doc(db, "cart", user.uid), (doc) => {
+        this.cart = doc.data();
+      });
+    },
+    addProducttoTable() {
+      this.profiledata.forEach((data) => {
+        for (let index = 0; index < this.cart.Product.length; index++) {
+          if (this.cart.Product[index].idproduct == data.id) {
+            this.producttable.push({
+              data: data.data.data,
+              img: data.data.linkfile,
+              count: this.cart.Product[index].total,
+            });
+            break;
+          }
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
